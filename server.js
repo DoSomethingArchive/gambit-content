439fc9edcb16f421096b19113da94e8cdb2296b9
Desmond Morris <hi@desmondmorris.com>
2014-06-18 10:33:05 -0400

    Adds loaderio verification file as static file

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 var application_root = __dirname
-    , express = require('express');
+    , express = require('express')
+    , path = require('path');
 
 /**
  * Express Setup
@@ -20,6 +21,9 @@ app.configure(function() {
 
   // Show all errors in development
   app.use(express.errorHandler({dumpException: true, showStack: true}));
+
+  // Add static path
+  app.use(express.static(path.join(__dirname, 'public')));
 });
 
 // Start server
@@ -27,33 +31,3 @@ var port = process.env.PORT || 4711;
 app.listen(port, function() {
   console.log('Express server listening on port %d in %s mode...\n\n', port, app.settings.env);
 });
-
-/**
- * Routes
- */
-
-/**
- * For loader.io load testing.
- * @TODO: move me to static file
- */
-if (process.env.LOADERIO_VERIFY_KEY) {
-  // loader.io verifies the domain by querying this location based off of the
-  // generated key that they provide.
-  app.get('/' + process.env.LOADERIO_VERIFY_KEY, function(req, res) {
-
-    // Return the key file provided by loader.io.
-    var filename = process.env.LOADERIO_VERIFY_KEY + '.txt';
-    require('fs').readFile(filename, "binary", function(err, file) {
-      if(err) {
-        res.writeHead(500, {"Content-Type": "text/plain"});
-        res.write(err + "\n");
-        res.end();
-        return;
-      }
-
-      res.writeHead(200);
-      res.write(file, "binary");
-      res.end();
-    });
-  });
-}
