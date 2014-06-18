b86b4b503eb595bed574c278d0543263d00a8ff1
Desmond Morris <hi@desmondmorris.com>
2014-06-18 13:50:00 -0400

    Auto loads configuration json files

--- app/config/index.js
+++ app/config/index.js
@@ -1,10 +1,9 @@
 var path = require('path');
+var fs = require('fs');
 var root_dirname = path.dirname(path.dirname(__dirname));
 
 module.exports = function(app, express) {
-
   app.configure(function() {
-
     // Set port variable
     app.set('port', process.env.PORT || 4711);
 
@@ -24,4 +23,19 @@ module.exports = function(app, express) {
     app.use(express.static(path.join(root_dirname, 'public')));
   });
 
+  fs.readdirSync('./app/config').forEach(function(file) {
+    if (file != path.basename(__filename)) {
+
+      var name = file.substr(0, file.lastIndexOf('.'))
+      var data = fs.readFileSync(root_dirname + '/app/config/' + file);
+
+      app.set(
+        name,
+        JSON.parse(data)
+      );
+
+      // Example:  app.get('mongo') will have the json object from app/config/mongo.json
+
+    }
+  });
 }
