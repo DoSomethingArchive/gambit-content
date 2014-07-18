962d60eba08516cafbd8029fb084f15ca125bcec
jon uy <jonxuy@gmail.com>
2014-07-18 18:17:30 -0400

    Adding mocha, Makefile, and start of a test script

---
+++ test/Game.js
@@ -0,0 +1,16 @@
+var assert = require('assert')
+  ;
+
+describe('End-to-end game playthrough:', function() {
+  describe('Creating a Competitive Story game', function() {
+    it('should add sg_user documents for all users')
+    it('should add a sg_competitivestory_game document')
+    it('should add a sg_gamemapping document')
+  })
+
+  describe('Beta 1 joining the game', function() {
+    it('should update the game document')
+    it('should send a Mobile Commons opt-in to Beta 1')
+    it('should send a Mobile Commons opt-in to Alpha')
+  })
+});
