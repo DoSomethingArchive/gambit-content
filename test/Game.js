97d11f5a84e5f737f44f3cb360ca42e7008822c2
jon uy <jonxuy@gmail.com>
2014-07-19 08:54:48 -0400

    Tests for verifying creation of documents after a createGame()

--- test/Game.js
+++ test/Game.js
@@ -34,25 +34,33 @@ describe('End-to-end game playthrough:', function() {
       };
 
       // Test request object to create the game.
+      alphaName = 'alpha';
+      alphaPhone = '5555550100';
+      betaName0 = 'friend0';
+      betaName1 = 'friend1';
+      betaName2 = 'friend2';
+      betaPhone0 = '5555550101';
+      betaPhone1 = '5555550102';
+      betaPhone2 = '5555550103';
       request = {
         body: {
           story_id: 1,
           person: {
-            first_name: 'alpha',
-            phone: '5555550100'
+            first_name: alphaName,
+            phone: alphaPhone
           },
           friends: [
             {
-              first_name: 'friend0',
-              phone: '5555550101'
+              first_name: betaName0,
+              phone: betaPhone0
             },
             {
-              first_name: 'friend1',
-              phone: '5555550102'
+              first_name: betaName1,
+              phone: betaPhone1
             },
             {
-              first_name: 'friend2',
-              phone: '5555550103'
+              first_name: betaName2,
+              phone: betaPhone2
             }
           ]
         }
@@ -86,12 +94,66 @@ describe('End-to-end game playthrough:', function() {
       });
 
       // With event listeners setup, can now create the game.
-      gameController.createGame(request, response);
+      assert.equal(true, gameController.createGame(request, response));
     })
 
-    it('should add sg_user documents for all users')
-    it('should add a sg_competitivestory_game document')
-    it('should add a sg_gamemapping document')
+    it('should add sg_user doc for alpha user', function(done) {
+      var phone = gameController.getNormalizedPhone(alphaPhone);
+      gameController.userModel.find({phone: phone}, function(err, docs) {
+        if (!err && docs.length > 0)
+          done();
+        else
+          assert(false);
+      })
+    })
+
+    it('should add sg_user doc for beta0 user', function(done) {
+      var phone = gameController.getNormalizedPhone(betaPhone0);
+      gameController.userModel.find({phone: phone}, function(err, docs) {
+        if (!err && docs.length > 0)
+          done();
+        else
+          assert(false);
+      })
+    })
+
+    it('should add sg_user doc for beta1 user', function(done) {
+      var phone = gameController.getNormalizedPhone(betaPhone1);
+      gameController.userModel.find({phone: phone}, function(err, docs) {
+        if (!err && docs.length > 0)
+          done();
+        else
+          assert(false);
+      })
+    })
+
+    it('should add sg_user doc for beta2 user', function(done) {
+      var phone = gameController.getNormalizedPhone(betaPhone2);
+      gameController.userModel.find({phone: phone}, function(err, docs) {
+        if (!err && docs.length > 0)
+          done();
+        else
+          assert(false);
+      })
+    })
+
+    it('should add a sg_gamemapping document', function(done) {
+      gameController.gameMappingModel.find({_id: gameMappingId}, function(err, docs) {
+        if (!err && docs.length > 0)
+          done();
+        else
+          assert(false);
+      })
+    })
+
+    it('should add a sg_competitivestory_game document', function(done) {
+      gameController.gameModel.find({_id: gameId}, function(err, docs) {
+        if (!err && docs.length > 0)
+          done();
+        else
+          assert(false);
+      })
+    })
   })
 
   describe('Beta 1 joining the game', function() {
@@ -102,10 +164,10 @@ describe('End-to-end game playthrough:', function() {
 
   after(function() {
       // Remove all test documents
-      gameController.userModel.remove({phone: '15555550100'}, function() {});
-      gameController.userModel.remove({phone: '15555550101'}, function() {});
-      gameController.userModel.remove({phone: '15555550102'}, function() {});
-      gameController.userModel.remove({phone: '15555550103'}, function() {});
+      gameController.userModel.remove({phone: gameController.getNormalizedPhone(alphaPhone)}, function() {});
+      gameController.userModel.remove({phone: gameController.getNormalizedPhone(betaPhone0)}, function() {});
+      gameController.userModel.remove({phone: gameController.getNormalizedPhone(betaPhone1)}, function() {});
+      gameController.userModel.remove({phone: gameController.getNormalizedPhone(betaPhone2)}, function() {});
       gameController.gameMappingModel.remove({_id: gameMappingId}, function() {});
       gameController.gameModel.remove({_id: gameId}, function() {});
     })
