f2a2fad74a31716af4d057efb529b002c5916dcf
Jon Uy <jonxuy@gmail.com>
2014-08-22 17:23:10 -0400

    Implemented logic to guide user through providing info to create an mp game from mobile

--- app/controllers/SGCreateFromMobileController.js
+++ app/controllers/SGCreateFromMobileController.js
@@ -1,13 +1,58 @@
+"use strict";
+
 /**
  * Guides users through creating an SMS multiplayer game from mobile.
  */
 
 var mobilecommons = require('../../mobilecommons/mobilecommons')
+  , messageHelper = require('../lib/userMessageHelpers')
   ;
 
 var SGCreateFromMobileController = function(app) {
   this.app = app;
   this.configModel = require('../models/sgGameCreateConfig')(app);
+};
+
+function createGame(gameConfig) {
+  // @todo POST to /sms-multiplayer-game/create
+  console.log('create the game!');
+};
+
+/**
+ * Helper function to normalize a potential phone number and check if it's valid.
+ *
+ * @param message
+ *   Message to check.
+ *
+ * @param Boolean
+ */
+function isPhoneNumber(message) {
+  return messageHelper.isValidPhone(messageHelper.getNormalizedPhone(message));
+};
+
+/**
+ * Subscribe phone number to a Mobile Commons opt-in path.
+ *
+ * @param phone
+ *  Phone number to subscribe.
+ * @param oip
+ *   Opt-in path to subscribe to.
+ */
+function sendSMS(phone, oip) {
+  var args = {
+    alphaPhone: phone,
+    alphaOptin: oip
+  };
+
+  mobilecommons.optin(args);
+};
+
+/**
+ * Custom error object used to break promise chain.
+ */
+function ErrorAbortPromiseChain() {
+  this.name = 'ErrorAbortPromiseChain';
+  this.message = 'Aborting the promise chain. This is totally OK.';
 };
 
 /**
@@ -44,11 +89,12 @@ SGCreateFromMobileController.prototype.processRequest = function(request, respon
   }
 
   var self = this;
+  self.storyConfig = this.gameConfig[request.query.story_id];
   self.request = request;
   self.response = response;
 
   // Query for an existing game creation config doc.
-  var queryConfig = this.configModel.findOne({phone: request.body.phone});
+  var queryConfig = this.configModel.findOne({alpha_mobile: request.body.phone});
   var promiseConfig = queryConfig.exec();
   promiseConfig.then(function(configDoc) {
 
@@ -64,17 +110,107 @@ SGCreateFromMobileController.prototype.processRequest = function(request, respon
 
       return self.configModel.create(doc);
     }
+    // If a document is found, then process the user message.
     else {
-      console.log('doc found');
-      console.log(configDoc)
+      var message = self.request.body.args;
+      // Create the game if we have at least one beta number
+      if (messageHelper.isYesResponse(message)) {
+        if (configDoc.beta_mobile_0 && messageHelper.isValidPhone(configDoc.beta_mobile_0)) {
+          createGame(configDoc);
+        }
+        else {
+          // Send the "not enough players" message.
+          sendSMS(configDoc.alpha_mobile, self.storyConfig.mobile_create.not_enough_players_oip);
+        }
+      }
+      else if (isPhoneNumber(message)) {
+        var betaMobile = messageHelper.getNormalizedPhone(message);
+
+        // If we haven't saved a beta number yet, save it to beta_mobile_0.
+        if (!configDoc.beta_mobile_0) {
+          configDoc.beta_mobile_0 = betaMobile;
+          self._updateDocument(configDoc);
+
+          // Then ask for beta_mobile_1.
+          sendSMS(configDoc.alpha_mobile, self.storyConfig.mobile_create.ask_beta_1_oip);
+        }
+        // If we haven't saved the 2nd beta number yet, save it to beta_mobile_1.
+        else if (!configDoc.beta_mobile_1) {
+          configDoc.beta_mobile_1 = betaMobile;
+          self._updateDocument(configDoc);
+
+          // Then ask for beta_mobile_2.
+          sendSMS(configDoc.alpha_mobile, self.storyConfig.mobile_create.ask_beta_2_oip);
+        }
+        // At this point, this is the last number we need. So, create the game.
+        else {
+          configDoc.beta_mobile_2 = betaMobile;
+          createGame(configDoc);
+        }
+      }
+      else {
+        // Send the "invalid response" message.
+        sendSMS(configDoc.alpha_mobile, self.storyConfig.mobile_create.invalid_mobile_oip);
+      }
+
+      throw new ErrorAbortPromiseChain();
+      return null;
     }
 
-  }).then(function(doc) {
-    // Config model has been successfully created
-    console.log(doc);
+  }).then(function(configDoc) {
+
+    // Config model has been successfully created.
+    if (configDoc) {
+      // User should have responded with beta_mobile_0.
+      var message = self.request.body.args;
+      if (isPhoneNumber(message)) {
+        // Update doc with beta_mobile_0 number.
+        // var doc = modelConfig._doc;
+        configDoc.beta_mobile_0 = messageHelper.getNormalizedPhone(message);
+        self._updateDocument(configDoc);
+
+        // Send next message asking for beta_mobile_1.
+        sendSMS(configDoc.alpha_mobile, self.storyConfig.mobile_create.ask_beta_1_oip);
+      }
+      else {
+        // If user responded with something else, ask for a valid phone number.
+        sendSMS(configDoc.alpha_mobile, self.storyConfig.mobile_create.invalid_mobile_oip);
+      }
+    }
+
+  }, function(err) {
+    // ErrorAbortPromiseChain is ok. Otherwise, log the error.
+    if (!(err instanceof ErrorAbortPromiseChain)) {
+      console.log(err);
+    }
   });
 
   response.send();
+};
+
+/**
+ * Update the game creation config doc with beta mobile numbers.
+ *
+ * @param configDoc
+ *   The document with updated data.
+ */
+SGCreateFromMobileController.prototype._updateDocument = function(configDoc) {
+  this.configModel.update(
+    {alpha_mobile: configDoc.alpha_mobile},
+    {$set: {
+      beta_mobile_0: configDoc.beta_mobile_0 ? configDoc.beta_mobile_0 : null,
+      beta_mobile_1: configDoc.beta_mobile_1 ? configDoc.beta_mobile_1 : null,
+      beta_mobile_2: configDoc.beta_mobile_2 ? configDoc.beta_mobile_2 : null,
+    }},
+    function(err, num, raw) {
+      if (err) {
+        console.log(err);
+      }
+      else {
+        console.log(raw);
+      }
+    }
+  );
 };
 
 module.exports = SGCreateFromMobileController;
