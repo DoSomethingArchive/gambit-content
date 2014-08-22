9f558fb817d4ab1e1607fc38d6d0084cfbeaa285
Jon Uy <jonxuy@gmail.com>
2014-08-22 11:57:44 -0400

    Creating a game config creation doc if none is found

--- app/controllers/SGCreateFromMobileController.js
+++ app/controllers/SGCreateFromMobileController.js
@@ -6,6 +6,7 @@ var mobilecommons = require('../../mobilecommons/mobilecommons')
   ;
 
 var SGCreateFromMobileController = function(app) {
+  this.app = app;
   this.configModel = require('../models/sgGameCreateConfig')(app);
 };
 
@@ -26,6 +27,52 @@ SGCreateFromMobileController.prototype.processRequest = function(request, respon
     response.send(406, 'Missing required params.');
     return false;
   }
+
+  // @todo When collaborative and most-likely-to games happen, set configs here.
+  // Verify game type and id are valid.
+  if (request.query.story_type == 'competitive-story') {
+    this.gameConfig = this.app.get('competitive-stories');
+  }
+  else {
+    response.send(406, 'Invalid story_type.')
+    return false;
+  }
+
+  if (typeof this.gameConfig[request.query.story_id] === 'undefined') {
+    response.send(406, 'Game config not setup for story ID: ' + request.query.story_id);
+    return false;
+  }
+
+  var self = this;
+  self.request = request;
+  self.response = response;
+
+  // Query for an existing game creation config doc.
+  var queryConfig = this.configModel.findOne({phone: request.body.phone});
+  var promiseConfig = queryConfig.exec();
+  promiseConfig.then(function(configDoc) {
+
+    // If no document found, then create one.
+    if (configDoc == null) {
+      // We don't ask for the first name yet, so just saving it as phone for now.
+      var doc = {
+        alpha_mobile: self.request.body.phone,
+        alpha_first_name: self.request.body.phone,
+        story_id: self.request.query.story_id,
+        story_type: self.request.query.story_type
+      };
+
+      return self.configModel.create(doc);
+    }
+    else {
+      console.log('doc found');
+      console.log(configDoc)
+    }
+
+  }).then(function(doc) {
+    // Config model has been successfully created
+    console.log(doc);
+  });
 
   response.send();
 };
