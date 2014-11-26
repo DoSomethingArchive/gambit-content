42ee694c78d34d0f8867ae98c330f38ff73f440b
Tong Xiang <tong.xiang2@gmail.com>
2014-11-26 15:19:03 -0500

    logicUserAction.js modularization

--- app/lib/sms-games/controllers/logicUserAction.js
+++ app/lib/sms-games/controllers/logicUserAction.js
@@ -21,7 +21,7 @@ var END_LEVEL_GROUP_MESSAGE_DELAY = 15000
  * forward in the story based on her answer.
  */
 module.exports = function(obj, doc) {
-
+  var gameDoc = doc;
   // Uppercase and only get first word of user's response.
   var userFirstWord = smsHelper.getFirstWord(obj.request.body.args.toUpperCase());
 
@@ -78,7 +78,6 @@ module.exports = function(obj, doc) {
     // Note: Sort of hacky, this needs to be called before message.end.level.indiv()
     // and message.end.game.indiv() because they use the story_results array in the
     // game document to determine what the next message should be.
-    var gameDoc = doc;
     gameDoc = record.updatedStoryResults(gameDoc, userPhone, currentOip, choiceKey);
 
     // Progress player to the next message.
@@ -94,7 +93,7 @@ module.exports = function(obj, doc) {
       gameDoc = record.updatedStoryResults(gameDoc, userPhone, nextOip);
 
       // Check if all players are waiting in an end-level state.
-      var readyForNextLevel = true;
+      var allPlayersReadyForNextLevel = true;
       for (var i = 0; i < gameDoc.players_current_status.length; i++) {
         // Skip this current user.
         if (gameDoc.players_current_status[i].phone == userPhone) {
@@ -111,102 +110,17 @@ module.exports = function(obj, doc) {
         }
 
         if (!playerAtEndLevel) {
-          readyForNextLevel = false;
+          allPlayersReadyForNextLevel = false;
           break;
         }
       }
 
-      // All players have reached the end of the level.
-      if (readyForNextLevel) {
-
-        /**
-         * Note: This probably isn't clear from just glancing at the code. The
-         * following two group messages are sent after a delay. But for this
-         * current user, she'll additionally be receiving the individual end
-         * level message first.
-         */
-
-        // Send group the end level message.
-        var endLevelGroupKey = level + '-GROUP';
-        var groupOptin = message.end.level.group(endLevelGroupKey, storyConfig, gameDoc);
-        for (var i = 0; i < gameDoc.players_current_status.length; i++) {
-          var playerPhone = gameDoc.players_current_status[i].phone;  
-
-          // Send group the end level message. The end level group message is sent 
-          // SECOND of all the messages in the execUserAction() function call.
-
-          message.singleUserWithDelay(playerPhone, groupOptin, END_LEVEL_GROUP_MESSAGE_DELAY, gameDoc._id, userModel);
-          gameDoc = record.updatedStoryResults(gameDoc, playerPhone, groupOptin);
-        }
-
-        // Note: Doing this `gameEnded` for loop separately from the end-level message so
-        // that results for all players can be updated before figuring out the
-        // final messages.
-        // Send group the next level message.
-        var gameEnded = false;
-        var nextLevel = storyConfig.story[endLevelGroupKey].next_level;
-        for (var i = 0; i < gameDoc.players_current_status.length; i++) {
-          var playerPhone = gameDoc.players_current_status[i].phone;
-          var nextPath = nextLevel;
-          // End game message needs to be determined per player
-          if (nextLevel == 'END-GAME') {
-            // If gameEnded is true; if this is the first player we're
-            // running endGame calculations on.
-            if (!gameEnded){
-              // Sends universal GROUP endgame message (sent THIRD,
-              // or second-last); updates gamedoc.
-              gameDoc = message.end.game.group(storyConfig, gameDoc);
-            }
-            gameEnded = true;
-            // This is setting the next OIP to the INDIVIDUAL end-game message.
-            nextPath = message.end.game.indiv(playerPhone, storyConfig, gameDoc);
-          }
-
-          // Sends individual user the next level message.
-          var optinArgs = {
-            alphaPhone: playerPhone,
-            alphaOptin: nextPath
-          };
-
-          // Sends individual user the next level message. The next level message
-          // (or if at end-game, end-game unique individual message)
-          // is sent LAST in the execUserAction() function call.
-          message.singleUserWithDelay(playerPhone, nextPath, NEXT_LEVEL_START_DELAY, gameDoc._id, userModel);
-
-          gameDoc = record.updatedStoryResults(gameDoc, playerPhone, nextPath);
-
-          // Update player's current status to the end game or next level message.
-          gameDoc = record.updatedPlayerStatus(gameDoc, playerPhone, nextPath);
-        }
+      if (allPlayersReadyForNextLevel) {
+        sendEndMessagesUpdateReturnGameDoc(level, gameDoc);
       }
     }
-
-    // If the game is over, log it to stathat.
-    if (gameEnded == true) {
-      gameDoc.game_ended = true;
-      utility.stathatReportCount(STATHAT_CATEGORY, 'end game', 'success', gameDoc.story_id, 1);
-    }
-
-    // Update the player's current status in the database. (Or ALL of the players' current statuses,
-    // if we're at the end-level or end-game situation.)
-    gameModel.update(
-      {_id: doc._id},
-      {$set: {
-        players_current_status: gameDoc.players_current_status,
-        story_results: gameDoc.story_results,
-        game_ended: gameDoc.game_ended
-      }},
-      function(err, num, raw) {
-        if (err) {
-          logger.error(err);
-        }
-        else {
-          emitter.emit('player-status-updated');
-          logger.info('SGCompetitiveStoryController.userAction - Updated player\'s',
-                      'current status in game doc:', doc._id.toString());
-        }
-      }
-    );
+    // Update the player's current status in the database.
+    dbUpdatePlayerStatus(gameDoc);
   }
   else {
     // If the user's response isn't a valid answer, 
@@ -214,10 +128,9 @@ module.exports = function(obj, doc) {
     nextOip = currentOip;
   }
 
-  // Send next immediate message via Mobile Commons opt in.
+  // The individual response message is sent FIRST
+  // in the logicUserAction() function call.
   if (userPhone && nextOip) {
-    // The individual response message is sent FIRST
-    // in the execUserAction() function call.
     message.singleUser(userPhone, nextOip);
 
     obj.response.send();
@@ -226,3 +139,104 @@ module.exports = function(obj, doc) {
     obj.response.status(500).send('Story configuration invalid.');
   }
 };
+
+/**
+ * Called if all users are at an end-level or end-game point. 
+ * Schedules and sends the appropriate messages, and then returns an updated gameDoc 
+ *
+ * @param level
+ *   The level for which the players have reached the end of. (Can also point to "END-GAME").
+ * @param gameDoc. 
+ *   The gameDoc, modified by previous code. 
+ * 
+ * returns a gameDoc with updated story results and player statuses. 
+ */
+
+function sendEndMessagesUpdateReturnGameDoc(level, gameDoc) {
+  /**
+   * Note: This probably isn't clear from just glancing at the code. The
+   * following two group messages are sent after a delay. But for this
+   * current user, she'll additionally be receiving the individual end
+   * level message first.
+   */
+
+  // Send group the end level message.
+  var endLevelGroupKey = level + '-GROUP';
+  var groupOptin = message.end.level.group(endLevelGroupKey, gameConfig[gameDoc.story_id], gameDoc);
+  for (var i = 0; i < gameDoc.players_current_status.length; i++) {
+    var playerPhone = gameDoc.players_current_status[i].phone;  
+
+    // Send group the end level message. The end level group message is sent 
+    // SECOND of all the messages in the logicUserAction() function call.
+
+    message.singleUserWithDelay(playerPhone, groupOptin, END_LEVEL_GROUP_MESSAGE_DELAY, gameDoc._id, userModel);
+    gameDoc = record.updatedStoryResults(gameDoc, playerPhone, groupOptin);
+  }
+
+  // Note: Doing this `gameEnded` for loop separately from the end-level message so
+  // that results for all players can be updated before figuring out the final messages.
+  // Send group the next level message.
+  var gameEnded = false;
+  var nextLevel = gameConfig[gameDoc.story_id].story[endLevelGroupKey].next_level;
+  for (var i = 0; i < gameDoc.players_current_status.length; i++) {
+    var playerPhone = gameDoc.players_current_status[i].phone;
+    var nextPath = nextLevel;
+    // End game message needs to be determined per player
+    if (nextLevel == 'END-GAME') {
+      // If gameEnded is true; if this is the first player we're
+      // running endGame calculations on.
+      if (!gameEnded){
+        // Sends universal GROUP endgame message (sent THIRD,
+        // or second-last); updates gamedoc.
+        gameDoc = message.end.game.group(gameConfig[gameDoc.story_id], gameDoc);
+      }
+      gameEnded = true;
+      gameDoc.game_ended = true;
+      utility.stathatReportCount(STATHAT_CATEGORY, 'end game', 'success', gameDoc.story_id, 1);
+      // This is setting the next OIP to the INDIVIDUAL end-game message.
+      nextPath = message.end.game.indiv(playerPhone, gameConfig[gameDoc.story_id], gameDoc);
+    }
+
+    // Sends individual user the next level message.
+    var optinArgs = {
+      alphaPhone: playerPhone,
+      alphaOptin: nextPath
+    };
+
+    // Sends individual user the next level message. The next level message
+    // (or if at end-game, end-game unique individual message)
+    // is sent LAST in the logicUserAction() function call.
+    message.singleUserWithDelay(playerPhone, nextPath, NEXT_LEVEL_START_DELAY, gameDoc._id, userModel);
+    gameDoc = record.updatedStoryResults(gameDoc, playerPhone, nextPath);
+    gameDoc = record.updatedPlayerStatus(gameDoc, playerPhone, nextPath);
+  }
+  return gameDoc;
+}
+
+/**
+ * Updates the gameDoc, with either the state changes resulting from one user 
+ * being at an end- game or level state, or all users being at that state. 
+ *
+ * @param gameDoc
+ *   gameDoc to be saved. 
+ */
+function dbUpdatePlayerStatus(gameDoc){
+  gameModel.update(
+    {_id: gameDoc._id},
+    {$set: {
+      players_current_status: gameDoc.players_current_status,
+      story_results: gameDoc.story_results,
+      game_ended: gameDoc.game_ended
+    }},
+    function(err, num, raw) {
+      if (err) {
+        logger.error(err);
+      }
+      else {
+        emitter.emit('player-status-updated');
+        logger.info('SGCompetitiveStoryController.userAction - Updated player\'s',
+                    'current status in game doc:', gameDoc._id.toString());
+      }
+    }
+  );
+}
