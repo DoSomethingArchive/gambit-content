667127c14d9567f9936198c02b078fa7059d4719
Tong Xiang <tong.xiang2@gmail.com>
2014-12-18 15:51:50 -0500

    fix seed.sh

--- seed.sh
+++ seed.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-if [ $WERCKER_MONGODB_HOST -a $WERCKER_MONGODB_PORT ]; then 
+# Evaluates if db env. variables are present by converting them to a string and evaluating non-zero length. 
+if [ -n "$WERCKER_MONGODB_HOST"] -a -n "$WERCKER_MONGODB_PORT" ]; then 
   mongoimport --host $WERCKER_MONGODB_HOST --port $WERCKER_MONGODB_PORT --db config --collection tips_configs < app/lib/ds-routing/config/tips.json --jsonArray
   mongoimport --host $WERCKER_MONGODB_HOST --port $WERCKER_MONGODB_PORT --db config --collection donorschoose_configs < app/lib/donations/config/donorschoose.json --jsonArray
 else 
