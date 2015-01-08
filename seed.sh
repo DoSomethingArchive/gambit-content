3c00ba6b450f255807226fbbccf5de4e3c6c0f78
Jon Uy <jonxuy@gmail.com>
2015-01-08 17:36:32 -0500

    Removing scripts that seeds production config db on the wercker deploy step - The thinking here is that now with the deploy_config.sh script, we should only have one method of updating the production db.

--- seed.sh
+++ seed.sh
@@ -1,21 +1,7 @@
 #!/bin/bash
 
 # Evaluates if db env. variables are present by converting them to a string and evaluating for non-zero length. 
-if [ -n "$CONFIG_DB_HOST" -a -n "$CONFIG_DB_PORT" -a -n "$CONFIG_DB_USERNAME" -a -n "$CONFIG_DB_PASSWORD" ]; then
-
-  mongoimport -u $CONFIG_DB_USERNAME -p $CONFIG_DB_PASSWORD --host $CONFIG_DB_HOST --port $CONFIG_DB_PORT --db config --collection tips < app/lib/ds-routing/config/tips.json --jsonArray
-
-  mongoimport -u $CONFIG_DB_USERNAME -p $CONFIG_DB_PASSWORD --host $CONFIG_DB_HOST --port $CONFIG_DB_PORT --db config --collection donorschoose < app/lib/donations/config/donorschoose.json --jsonArray
-
-  mongoimport -u $CONFIG_DB_USERNAME -p $CONFIG_DB_PASSWORD --host $CONFIG_DB_HOST --port $CONFIG_DB_PORT --db config --collection campaign_start < app/lib/ds-routing/config/campaign-start.json --jsonArray
-
-  mongoimport -u $CONFIG_DB_USERNAME -p $CONFIG_DB_PASSWORD --host $CONFIG_DB_HOST --port $CONFIG_DB_PORT --db config --collection start_campaign_transitions < app/lib/ds-routing/config/start-campaign-transitions.json --jsonArray
-
-  mongoimport -u $CONFIG_DB_USERNAME -p $CONFIG_DB_PASSWORD --host $CONFIG_DB_HOST --port $CONFIG_DB_PORT --db config --collection yes_no_paths < app/lib/ds-routing/config/yes-no-paths.json --jsonArray
-
-  mongoimport -u $CONFIG_DB_USERNAME -p $CONFIG_DB_PASSWORD --host $CONFIG_DB_HOST --port $CONFIG_DB_PORT --db config --collection competitive_stories < app/lib/sms-games/config/competitive-stories.json --jsonArray
-
-elif [ -n "$WERCKER_MONGODB_HOST" -a -n "$WERCKER_MONGODB_PORT" ]; then 
+if [ -n "$WERCKER_MONGODB_HOST" -a -n "$WERCKER_MONGODB_PORT" ]; then
 
   mongoimport --host $WERCKER_MONGODB_HOST --port $WERCKER_MONGODB_PORT --db config --collection tips < app/lib/ds-routing/config/tips.json --jsonArray
 
