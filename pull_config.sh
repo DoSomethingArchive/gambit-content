3090aab93dcbe0d93e69ecb08991e066e7f721ea
Jon Uy <jonxuy@gmail.com>
2015-01-12 16:38:48 -0500

    Deleting push script and config/*.json files. Pull script changed to export .json files on local fs.

--- pull_config.sh
+++ pull_config.sh
@@ -18,23 +18,28 @@ echo
 
 if [[ $REPLY =~ ^[Yy]$ ]]; then
   # Source environment variables
-  source ./.deploy_config.conf
+  source ./.pull_config.conf
 
   if [ -n "$SMS_CONFIG_DB_HOST" ] && \
      [ -n "$SMS_CONFIG_DB_PORT" ] && \
      [ -n "$SMS_CONFIG_DB_USERNAME" ] && \
      [ -n "$SMS_CONFIG_DB_PASSWORD" ]; then
   
-    # Dump collections from production
-    mongodump --host $SMS_CONFIG_DB_HOST --port $SMS_CONFIG_DB_PORT --db config -u $SMS_CONFIG_DB_USERNAME -p $SMS_CONFIG_DB_PASSWORD -c tips
-    mongodump --host $SMS_CONFIG_DB_HOST --port $SMS_CONFIG_DB_PORT --db config -u $SMS_CONFIG_DB_USERNAME -p $SMS_CONFIG_DB_PASSWORD -c donorschoose
-    mongodump --host $SMS_CONFIG_DB_HOST --port $SMS_CONFIG_DB_PORT --db config -u $SMS_CONFIG_DB_USERNAME -p $SMS_CONFIG_DB_PASSWORD -c campaign_start
-    mongodump --host $SMS_CONFIG_DB_HOST --port $SMS_CONFIG_DB_PORT --db config -u $SMS_CONFIG_DB_USERNAME -p $SMS_CONFIG_DB_PASSWORD -c start_campaign_transitions
-    mongodump --host $SMS_CONFIG_DB_HOST --port $SMS_CONFIG_DB_PORT --db config -u $SMS_CONFIG_DB_USERNAME -p $SMS_CONFIG_DB_PASSWORD -c yes_no_paths
-    mongodump --host $SMS_CONFIG_DB_HOST --port $SMS_CONFIG_DB_PORT --db config -u $SMS_CONFIG_DB_USERNAME -p $SMS_CONFIG_DB_PASSWORD -c competitive_stories
+    # Export collections from production
+    mongoexport --host $SMS_CONFIG_DB_HOST --port $SMS_CONFIG_DB_PORT --db config -u $SMS_CONFIG_DB_USERNAME -p $SMS_CONFIG_DB_PASSWORD --jsonArray -c tips -o app/lib/ds-routing/config/tips.json
+    mongoexport --host $SMS_CONFIG_DB_HOST --port $SMS_CONFIG_DB_PORT --db config -u $SMS_CONFIG_DB_USERNAME -p $SMS_CONFIG_DB_PASSWORD --jsonArray -c donorschoose -o app/lib/donations/config/donorschoose.json
+    mongoexport --host $SMS_CONFIG_DB_HOST --port $SMS_CONFIG_DB_PORT --db config -u $SMS_CONFIG_DB_USERNAME -p $SMS_CONFIG_DB_PASSWORD --jsonArray -c campaign_start -o app/lib/ds-routing/config/campaign-start.json
+    mongoexport --host $SMS_CONFIG_DB_HOST --port $SMS_CONFIG_DB_PORT --db config -u $SMS_CONFIG_DB_USERNAME -p $SMS_CONFIG_DB_PASSWORD --jsonArray -c start_campaign_transitions -o app/lib/ds-routing/config/start-campaign-transitions.json
+    mongoexport --host $SMS_CONFIG_DB_HOST --port $SMS_CONFIG_DB_PORT --db config -u $SMS_CONFIG_DB_USERNAME -p $SMS_CONFIG_DB_PASSWORD --jsonArray -c yes_no_paths -o app/lib/ds-routing/config/yes-no-paths.json
+    mongoexport --host $SMS_CONFIG_DB_HOST --port $SMS_CONFIG_DB_PORT --db config -u $SMS_CONFIG_DB_USERNAME -p $SMS_CONFIG_DB_PASSWORD --jsonArray -c competitive_stories -o app/lib/sms-games/config/competitive-stories.json
   
-    # Then restore to currently running mongod
-    mongorestore
+    # Then import them to the currently running mongod
+    mongoimport --db config --jsonArray -c tips --file app/lib/ds-routing/config/tips.json
+    mongoimport --db config --jsonArray -c donorschoose --file app/lib/donations/config/donorschoose.json
+    mongoimport --db config --jsonArray -c campaign_start --file app/lib/ds-routing/config/campaign-start.json
+    mongoimport --db config --jsonArray -c start_campaign_transitions --file app/lib/ds-routing/config/start-campaign-transitions.json
+    mongoimport --db config --jsonArray -c yes_no_paths --file app/lib/ds-routing/config/yes-no-paths.json
+    mongoimport --db config --jsonArray -c competitive_stories --file app/lib/sms-games/config/competitive-stories.json
 
     echo "\nLocal database updated with config from production!\n"
   fi
