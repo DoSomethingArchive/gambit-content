0dc051950b72c16f544b8908d860e7e38a6c7468
Jon Uy <jonxuy@gmail.com>
2015-01-12 17:35:25 -0500

    Modifying seed.sh with error message. Should only be used by wercker now.

--- seed.sh
+++ seed.sh
@@ -36,16 +36,12 @@ if [ -n "$SMS_CONFIG_DB_HOST" ] && \
 
 else
 
-  mongoimport --db config --collection donorschoose < app/lib/donations/config/donorschoose.json --jsonArray
-
-  mongoimport --db config --collection tips < app/lib/ds-routing/config/tips.json --jsonArray
-
-  mongoimport --db config --collection campaign_start < app/lib/ds-routing/config/campaign-start.json --jsonArray
-
-  mongoimport --db config --collection start_campaign_transitions < app/lib/ds-routing/config/start-campaign-transitions.json --jsonArray
-
-  mongoimport --db config --collection yes_no_paths < app/lib/ds-routing/config/yes-no-paths.json --jsonArray
-
-  mongoimport --db config --collection competitive_stories < app/lib/sms-games/config/competitive-stories.json --jsonArray
+  echo "Error - At least one of these env vars isn't set:"
+  echo "  - SMS_CONFIG_DB_HOST"
+  echo "  - SMS_CONFIG_DB_PORT"
+  echo "  - SMS_CONFIG_DB_USERNAME"
+  echo "  - SMS_CONFIG_DB_PASSWORD"
+  echo "  - WERCKER_MONGODB_HOST"
+  echo "  - $WERCKER_MONGODB_PORT"
 
 fi
