581343b8d0e20708eb1bec56ba411bc1002b6917
Aaron Schachter <aaronschachter@gmail.com>
2016-07-18 17:39:04 -0700

    Opts member into hardcoded OIP upon submitting

--- app/lib/puppet-sloth/index.js
+++ app/lib/puppet-sloth/index.js
@@ -5,6 +5,7 @@
 var express = require('express')
   , router = express.Router()
   , mobilecommons = rootRequire('mobilecommons')
+  , smsHelper = rootRequire('app/lib/smsHelpers')
   , logger = rootRequire('app/lib/logger')
   ;
 
@@ -14,8 +15,14 @@ var express = require('express')
  *
  */
 router.post('/:food', function(request, response) {
-  console.log("Puppet time");
+  phone = smsHelper.getNormalizedPhone(request.body.phone);
+  console.log(phone + " has requested puppet time for " + request.body.args);
   response.send('Next question!');
+  var args = {
+    alphaPhone: phone,
+    alphaOptin: 210045
+  };
+  mobilecommons.optin(args);
 });
 
 module.exports = router;
