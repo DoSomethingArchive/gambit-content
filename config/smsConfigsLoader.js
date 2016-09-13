8f6f227e1b0ae257fd6539645e052dd54271c696
Aaron Schachter <aaronschachter@gmail.com>
2016-09-13 15:26:46 -0700

    Adds CampaignBot model

--- config/smsConfigsLoader.js
+++ config/smsConfigsLoader.js
@@ -3,6 +3,7 @@
  */
 app.ConfigName = {
   CAMPAIGN_TRANSITIONS: 'start_campaign_transition',
+  CAMPAIGNBOTS: 'campaignbots',
   CAMPAIGNS: 'campaigns',
   CHATBOT_MOBILECOMMONS_CAMPAIGNS: 'chatbot_mobilecommons_campaigns',
   DONORSCHOOSE_BOTS: 'donorschoose_bots',
@@ -14,6 +15,7 @@ var conn = require('./connectionConfig');
 var configModelArray = [
   rootRequire('api/models/ChatbotMobileCommonsCampaign')(conn),
   rootRequire('api/models/campaign/Campaign')(conn),
+  rootRequire('api/models/campaign/CampaignBot')(conn),
   rootRequire('api/models/donation/DonorsChooseBot')(conn),
   rootRequire('api/legacy/ds-routing/config/startCampaignTransitionsConfigModel')(conn),
   rootRequire('api/legacy/ds-routing/config/yesNoPathsConfigModel')(conn),
