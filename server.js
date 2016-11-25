baf1b4c25777a1d846d7442c45106f0119eb4d06
deadlybutter <thedeadlybutter@gmail.com>
2016-11-25 17:50:38 -0500

    Removing unused line

--- server.js
+++ server.js
@@ -124,8 +124,6 @@ if (!app.locals.clients.phoenix) {
   process.exit(1);
 }
 
-app.locals.rabbit = require('./lib/rabbit');
-
 conn.on('connected', () => {
   logger.info(`conn.readyState:${conn.readyState}`);
 
