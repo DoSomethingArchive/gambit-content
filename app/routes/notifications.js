3ea88638c432ca4bc74e4b5bb4e5e9a75ae8a85c
deadlybutter <thedeadlybutter@gmail.com>
2016-11-30 14:43:16 -0500

    Adds reminder endpoint

--- app/routes/notifications.js
+++ app/routes/notifications.js
@@ -2,19 +2,35 @@
 
 const express = require('express');
 const router = express.Router(); // eslint-disable-line new-cap
+const mobilecommons = rootRequire('lib/mobilecommons');
+const logger = app.locals.logger;
+const stathat = app.locals.stathat;
 
-router.post('/', (req, res) => {
+router.post('/reminder', (req, res) => {
   const northstarUserId = req.body.northstarUserId;
-  const message = req.body.message;
-  const northstar = app.locals.clients.northstar;
+  const campaignId = req.body.campaignId;
 
-  if (!northstarUserId || !message) {
+  if (!northstarUserId || !campaignId) {
     res.json({error: 'Missing parameters'});
     return;
   }
 
-  northstar.Users.get('id', northstarUserId).then(console.log);
+  app.locals.clients.northstar.Users
+  .get('id', northstarUserId)
+  .then(nsUser => nsUser.mobile)
+  .then((mobile) => {
+    if (!mobile) return res.json({error: 'No mobile number for NS user.'});
 
+    const reminderMessage = app.locals.campaigns[campaignId].msg_relative_reminder;
+    if (!reminderMessage) return res.json({error: 'No reminder set for campaign.'});
+
+    mobilecommons.send_message(mobile, reminderMessage);
+    res.json({success: true});
+    stathat('Sent relative reminder');
+  })
+  .catch((err) => {
+    logger.error('Error sending reminder message', err);
+  });
 });
 
 module.exports = router;
