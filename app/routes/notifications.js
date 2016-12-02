0379557775a8354ca3bd9ee9189d475b8eced987
Sergii Tkachenko <hi@sergii.org>
2016-12-02 15:24:42 -0500

    Reminders: change input param to mobile, add logging.

--- app/routes/notifications.js
+++ app/routes/notifications.js
@@ -2,41 +2,65 @@
 
 const express = require('express');
 const router = express.Router(); // eslint-disable-line new-cap
+const helpers = require('../../lib/helpers');
+
 const mobilecommons = rootRequire('lib/mobilecommons');
 const logger = app.locals.logger;
 const stathat = app.locals.stathat;
 
+const allowedReminderTypes = ['signup', 'reportback'];
+
 router.post('/reminder', (req, res) => {
-  const northstarUserId = req.body.northstarUserId;
-  const campaignId = req.body.campaignId;
-  const reminderType = req.body.reminderType;
-
-  if (!northstarUserId || !campaignId || !reminderType) {
-    res.json({ error: 'Missing parameters' });
-    return;
+  // Check required parameters.
+  if (!req.body.mobile) {
+    return helpers.sendResponse(res, 422, 'Missing required mobile parameter.');
+  }
+  if (!req.body.campaign_id) {
+    return helpers.sendResponse(res, 422, 'Missing required campaign id parameter.');
+  }
+  if (!req.body.reminder_type) {
+    return helpers.sendResponse(res, 422, 'Missing required reminder type parameter.');
   }
 
-  if (!app.locals.campaigns[campaignId]) {
-    res.json({ error: 'No campaign set in Gambit for the given campaign id.' });
-    return;
+  const mobile = req.body.mobile;
+  const campaignID = req.body.campaign_id;
+  const reminderType = req.body.reminder_type;
+  const reminderCampaignField = `msg_relative_reminder_${reminderType}`;
+
+  // Check allowed reminder types.
+  if (allowedReminderTypes.indexOf(reminderType) === -1) {
+    const msg = `Reminder type should be one of: ${allowedReminderTypes.join(', ')}, got: ${reminderType}`;
+    return helpers.sendResponse(res, 422, msg);
   }
 
-  app.locals.clients.northstar.Users
-  .get('id', northstarUserId)
-  .then((nsUser) => { // eslint-disable-line consistent-return
-    const mobile = nsUser.mobile;
-    if (!mobile) return res.json({ error: 'No mobile number for NS user.' });
+  // Check that campaign is active.
+  const campaign = app.locals.campaigns[campaignID];
+  if (!campaign || campaign.status !== 'active') {
+    const msg = `Campaign ${campaignID} is not running on CampaignBot`;
+    return helpers.sendResponse(res, 422, msg);
+  }
 
-    const reminderParam = `msg_relative_reminder_${reminderType}`;
-    const reminderMessage = app.locals.campaigns[campaignId][reminderParam];
-    if (!reminderMessage) return res.json({ error: 'No reminder set for campaign.' });
+  // Check that campaign suports requested reminder type.
+  const reminderMessage = campaign[reminderCampaignField]
+  if (!reminderMessage) {
+    const msg = `Campaign ${campaignID} does not support reminders for '${reminderType}'`;
+    return helpers.sendResponse(res, 422, msg);
+  }
 
+  app.locals.clients.northstar.Users.get('mobile', mobile)
+  .then((user) => {
     mobilecommons.send_message(mobile, reminderMessage);
+    const msg = `Sent reminder for ${campaignID} ${reminderType} to ${mobile}`;
+    logger.info(msg);
     stathat('Sent relative reminder');
-    res.json({ success: true });
+    return res.json({ success: true });
   })
   .catch((err) => {
-    logger.error('Error sending reminder message', err.message);
+    if (err.response) {
+      logger.error(err.response.error);
+    }
+    const msg = `Error sending reminder to user #${mobile}: ${err.message}`;
+    return helpers.sendResponse(res, 500, msg);
   });
 });
 
