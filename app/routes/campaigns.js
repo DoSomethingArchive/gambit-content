81c3c6cc4f35c60608343a3655b1d7e389cc4cf9
Sergii Tkachenko <hi@sergii.org>
2016-12-05 15:34:55 -0500

    Remove allowedTypes messages logic (schema does that).

--- app/routes/campaigns.js
+++ app/routes/campaigns.js
@@ -48,48 +48,38 @@ router.get('/:id', (req, res) => {
 
 router.post('/:id/message', (req, res) => {
   // Check required parameters.
-  const campaignID = req.params.id;
+  const campaignId = req.params.id;
   const mobile = req.body.mobile;
   const type = req.body.type;
 
   if (!mobile) {
     return helpers.sendResponse(res, 422, 'Missing required mobile parameter.');
   }
-  if (!campaignID) {
+  if (!campaignId) {
     return helpers.sendResponse(res, 422, 'Missing required campaign parameter.');
   }
   if (!type) {
     return helpers.sendResponse(res, 422, 'Missing required message type parameter.');
   }
 
-  // Check allowed message types.
-  const allowedTypes = [
-    'signup',
-    'reportback'
-  ];
-  if (allowedTypes.indexOf(type) === -1) {
-    const msg = `Reminder type should be one of: ${allowedTypes}, got: ${type}`;
-    return helpers.sendResponse(res, 422, msg);
-  }
-
   // Check that campaign is active.
-  const campaign = app.locals.campaigns[campaignID];
+  const campaign = app.locals.campaigns[campaignId];
   if (!campaign || campaign.status !== 'active') {
-    const msg = `Campaign ${campaignID} is not running on CampaignBot`;
+    const msg = `Campaign ${campaignId} is not running on CampaignBot`;
     return helpers.sendResponse(res, 422, msg);
   }
 
   // Check that campaign suports requested message type.
   const messageBody = campaign.messages[type];
   if (!messageBody) {
-    const msg = `Campaign ${campaignID} does not support reminders for '${type}'`;
+    const msg = `Campaign ${campaignId} does not support reminders for '${type}'`;
     return helpers.sendResponse(res, 422, msg);
   }
 
   return app.locals.clients.northstar.Users.get('mobile', mobile)
   .then((user) => {
     mobilecommons.send_message(mobile, messageBody);
-    const msg = `Sent text for ${campaignID} ${type} to ${user.mobile}`;
+    const msg = `Sent text for ${campaignId} ${type} to ${user.mobile}`;
     logger.info(msg);
     stathat('Sent relative text');
     return res.json({ success: true });
