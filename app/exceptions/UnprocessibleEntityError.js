0633d2bb919d95ec756ef5e723b8eb7dd125e4c1
Aaron Schachter <aaronschachter@gmail.com>
2017-04-17 12:34:31 -0700

    Adds status property and sets as 422

--- app/exceptions/UnprocessibleEntityError.js
+++ app/exceptions/UnprocessibleEntityError.js
@@ -1,5 +1,12 @@
 'use strict';
 
-class UnprocessibleEntityError extends Error {}
+class UnprocessibleEntityError extends Error {
+
+  constructor(message) {
+    super(message);
+    this.status = 422;
+  }
+
+}
 
 module.exports = UnprocessibleEntityError;
