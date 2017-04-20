adade304c32281bbf46f6db776a0650324fe441b
Rafa <rapala61@gmail.com>
2017-04-20 17:05:17 -0400

    style changes to stop lint errors

--- newrelic.js
+++ newrelic.js
@@ -1,16 +1,18 @@
+'use strict';
+
 /**
  * New Relic agent configuration.
  *
  */
 exports.config = {
-  app_name : [process.env.NEW_RELIC_APP_NAME],
-  license_key : process.env.NEW_RELIC_LICENSE_KEY,
-  logging : {
+  app_name: [process.env.NEW_RELIC_APP_NAME],
+  license_key: process.env.NEW_RELIC_LICENSE_KEY,
+  logging: {
     /**
      * Level at which to log. 'trace' is most useful to New Relic when diagnosing
      * issues with the agent, 'info' and higher will impose the least overhead on
      * production applications.
      */
-    level : process.env.NEW_RELIC_LOGGING_LEVEL
-  }
-}
+    level: process.env.NEW_RELIC_LOGGING_LEVEL,
+  },
+};
