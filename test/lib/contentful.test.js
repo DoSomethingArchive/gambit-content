dac90385028e76bd04128009d62080ed040c54f1
Rafa <rapala61@gmail.com>
2017-05-02 14:27:44 -0400

    started implementation of lib/contentful tests

--- test/lib/contentful.test.js
+++ test/lib/contentful.test.js
@@ -1,8 +1,82 @@
 'use strict';
 
 require('dotenv').config();
-// const contentful = require('../../lib/contentful');
+const Promise = require('bluebird');
 const test = require('ava');
-// const chai = require('chai');
+const chai = require('chai');
+// const expect = require('chai').expect;
+const stubs = require('../../test/utils/stubs');
+const sinon = require('sinon');
+const sinonChai = require('sinon-chai');
+const httpMocks = require('node-mocks-http');
+const stathat = require('../../lib/stathat');
+const logger = require('winston');
+const contentfulAPI = require('contentful');
+const rewire = require('rewire');
 
-test.todo('placeholder');
+// setup "x.should.y" assertion style
+chai.should();
+chai.use(sinonChai);
+
+// module to be tested
+const contentful = rewire('../../lib/contentful');
+
+const sandbox = sinon.sandbox.create();
+
+// Stub functions
+const clientFailObject = { space: 'fail', accessToken: 'fail' };
+const entriesResponseStub = () => Promise.resolve(stubs.getEntries());
+const entriesResponseStubFail = () => Promise.reject({ status: 500 });
+const getEntriesStub = sinon.stub()
+  .callsFake(entriesResponseStub)
+  .withArgs(clientFailObject)
+  .callsFake(entriesResponseStubFail);
+const contentfulAPIStub = {
+  getEntries: getEntriesStub,
+};
+
+// Setup!
+test.beforeEach((t) => {
+  sandbox.stub(logger, 'error');
+  sandbox.stub(logger, 'debug');
+  sandbox.stub(stathat, 'postStat');
+  sandbox.stub(contentfulAPI, 'createClient')
+    .returns(contentfulAPIStub);
+
+  // setup req, res mocks
+  t.context.req = httpMocks.createRequest();
+  t.context.res = httpMocks.createResponse();
+});
+
+// Cleanup!
+test.afterEach((t) => {
+  // reset stubs, spies, and mocks
+  sandbox.restore();
+  t.context = {};
+});
+
+test('createNewClient', () => {
+  const client = contentful.createNewClient();
+  contentfulAPI.createClient.should.have.been.called;
+  client.should.respondTo('getEntries');
+});
+
+test('getClient should not call createClient if client exists', () => {
+  const client = contentful.getClient();
+  contentfulAPI.createClient.should.not.have.been.called;
+  client.should.respondTo('getEntries');
+});
+
+test('getClient should call createClient if client doesn\'t exist', () => {
+  const restore = contentful.__set__('client', undefined);
+  const client = contentful.getClient();
+  contentfulAPI.createClient.should.have.been.called;
+  client.should.respondTo('getEntries');
+  restore();
+});
+
+test('contentfulError', () => {
+  const errorObj = { message: 'tacos' };
+  const error = contentful.contentfulError(errorObj);
+  error.message.should.have.string(errorObj.message);
+});
