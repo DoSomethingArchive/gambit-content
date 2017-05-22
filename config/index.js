55ecbf31e82e2d6dad93dc123a6c3157f1de180e
Rafa <rapala61@gmail.com>
2017-05-22 16:51:55 -0400

    main config file that is used in server.js

---
+++ config/index.js
@@ -0,0 +1,37 @@
+'use strict';
+
+/* eslint-disable import/no-dynamic-require */
+/* eslint-disable global-require */
+
+// @see https://docs.newrelic.com/docs/agents/nodejs-agent/installation-configuration
+require('newrelic');
+
+// Wrapper around require to set relative path at app root
+global.rootRequire = function (name) {
+  return require(`../${name}`);
+};
+
+const configVars = {};
+configVars.environment = process.env.NODE_ENV || 'development';
+configVars.port = process.env.PORT || 5000;
+configVars.webConcurrency = process.env.WEB_CONCURRENCY || 1;
+configVars.dbUri = process.env.DB_URI || 'mongodb://localhost/ds-mdata-responder';
+
+/**
+ * Winston Logger setup
+ * We don't need to export the Logger. Just require adhoc in whatever file we need it,
+ * like: const logger = require('winston');
+ */
+require('./logger')({
+  dbUri: configVars.dbUri,
+});
+
+/**
+ * Mongo connection setup
+ * export mongooseConnection
+ */
+configVars.mongooseConnection = require('./mongoose')({
+  dbUri: configVars.dbUri,
+});
+
+module.exports = configVars;
