ba3a818e162ac653c489cc688f2fc1342e813da2
Rafa <rapala61@gmail.com>
2017-06-04 20:10:42 -0400

    modified way to load timeouts middleware

--- app.js
+++ app.js
@@ -5,6 +5,9 @@ const favicon = require('serve-favicon');
 const path = require('path');
 const bodyParser = require('body-parser');
 
+// middleware
+const timeoutMiddleware = require('./lib/middleware/timeouts');
+
 const app = express();
 // serve favicon
 app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
@@ -13,7 +16,9 @@ app.use(bodyParser.json());
 // parse application/x-www-form-urlencoded Content-Type
 app.use(bodyParser.urlencoded({ extended: true }));
 // 504 timeouts middleware
-require('./lib/middleware/timeouts')(app);
+Object.keys(timeoutMiddleware).forEach((name) => {
+  app.use(timeoutMiddleware[name]);
+});
 // require all routes
 require('./app/routes')(app);
 
