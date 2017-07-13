c2d7cbe8cd5c934a70d7d5663c9da49fb16a5621
Aaron Schachter <aaronschachter@gmail.com>
2017-07-13 15:12:16 -0700

    Adds nock

--- config/lib/consolebot.js
+++ config/lib/consolebot.js
@@ -1,7 +1,12 @@
 'use strict';
 
+require('dotenv').config();
+const appConfig = require('../');
+
 const configVars = {
+  apiKey: appConfig.apiKey,
   phone: process.env.GAMBIT_CONSOLEBOT_PHONE || 'consolebot',
+  url: `http://localhost:${appConfig.port}/v1/chatbot`,
 };
 
 module.exports = configVars;
--- test/lib/consolebot.test.js
+++ test/lib/consolebot.test.js
@@ -9,6 +9,7 @@ const logger = require('winston');
 const sinonChai = require('sinon-chai');
 const httpMocks = require('node-mocks-http');
 const sinon = require('sinon');
+const nock = require('nock');
 
 const stubs = require('../utils/stubs.js');
 const config = require('../../config/lib/consolebot');
@@ -47,13 +48,28 @@ test('consolebot should respond to post', () => {
 });
 test('consolebot should respond to start', () => {
   const consolebot = new Consolebot(config);
-  consolebot.should.respondTo('post');
+  consolebot.should.respondTo('start');
 });
 test('consolebot should respond to reply', () => {
   const consolebot = new Consolebot(config);
-  consolebot.should.respondTo('post');
+  consolebot.should.respondTo('reply');
 });
-test('consolebot should respond to print', () => {
+
+test('consolebot should call print on post', async () => {
+  // setup
   const consolebot = new Consolebot(config);
-  consolebot.should.respondTo('post');
+  const msgType = 'hello!';
+  nock(config.url)
+    .post('', {})
+    // nock will match any query params
+    .query(true)
+    .reply(200, {
+      success: {
+        message: 'Howdy.',
+      },
+    });
+
+  // test
+  await consolebot.post(msgType);
+  Consolebot.print.should.have.been.called;
 });
