b345c3079394f649defd646c8b41c864a5523c9f
Aaron Schachter <aaronschachter@gmail.com>
2017-07-13 19:49:25 -0700

    More config options

--- config/lib/consolebot.js
+++ config/lib/consolebot.js
@@ -6,6 +6,9 @@ const appConfig = require('../');
 const configVars = {
   apiKey: appConfig.apiKey,
   phone: process.env.GAMBIT_CONSOLEBOT_PHONE || 'consolebot',
+  prompt: process.env.GAMBIT_CONSOLEBOT_PROMPT || 'You>',
+  replyPrefix: process.env.GAMBIT_CONSOLEBOT_REPLY_PREFIX || 'Bot>',
+  replyColor: process.env.GAMBIT_CONSOLEBOT_REPLY_COLOR || 'magenta',
   url: `http://localhost:${appConfig.port}/v1/chatbot`,
 };
 
