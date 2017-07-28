727f34e2a484c74917b59f464af94bf422654b76
Aaron Schachter <aaronschachter@gmail.com>
2017-07-28 12:43:56 -0700

    Adds photo command to Consolebot

--- config/lib/consolebot/index.js
+++ config/lib/consolebot/index.js
@@ -5,11 +5,15 @@ const path = require('path');
 require('dotenv').config();
 const appConfig = require('../../');
 
+// Used as a Reportback image.
+const photoUrl = 'https://www.wired.com/wp-content/uploads/2015/03/The-X-Files1-1024x768.jpg';
+
 const configVars = {
   apiKey: appConfig.apiKey,
   introFilePath: path.resolve(__dirname, 'intro.txt'),
   phone: process.env.GAMBIT_CONSOLEBOT_PHONE,
   phoneUndefinedMessage: 'process.env.GAMBIT_CONSOLEBOT_PHONE undefined',
+  photoUrl: process.env.GAMBIT_CONSOLEBOT_PHOTO_URL || photoUrl,
   prompt: process.env.GAMBIT_CONSOLEBOT_PROMPT || 'You>',
   replyPrefix: process.env.GAMBIT_CONSOLEBOT_REPLY_PREFIX || 'Bot>',
   replyColor: process.env.GAMBIT_CONSOLEBOT_REPLY_COLOR || 'magenta',
