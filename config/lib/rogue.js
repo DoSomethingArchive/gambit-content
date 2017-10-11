e938f18516f5d0d104f254d3105c6bbdd6087da1
Aaron Schachter <aaronschachter@gmail.com>
2017-10-10 22:22:06 -0700

    Deprecates helpers.getCampaignActivitySource

--- config/lib/rogue.js
+++ config/lib/rogue.js
@@ -1,12 +1,10 @@
 'use strict';
 
-const helpers = require('../../lib/helpers');
-
 module.exports = {
   apiKeyHeader: 'X-DS-Rogue-API-Key',
   clientOptions: {
     baseUri: process.env.DS_ROGUE_API_BASEURI,
     apiKey: process.env.DS_ROGUE_API_KEY,
   },
-  source: helpers.getCampaignActivitySource(),
+  source: process.env.DS_API_POST_SOURCE || 'sms-mobilecommons',
 };
