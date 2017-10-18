e514ec28551b488649f42fdd5499575e5d389934
Aaron Schachter <aaronschachter@gmail.com>
2017-10-17 20:46:04 -0700

    Starts on Phoenix tests

--- test/lib/phoenix.test.js
+++ test/lib/phoenix.test.js
@@ -1,8 +1,35 @@
 'use strict';
 
 require('dotenv').config();
-// const phoenix = require('../../lib/phoenix');
-const test = require('ava');
-// const chai = require('chai');
 
-test.todo('placeholder');
+const test = require('ava');
+const chai = require('chai');
+const sinonChai = require('sinon-chai');
+const sinon = require('sinon');
+/*
+const config = require('../../config/lib/phoenix');
+const baseUri = config.clientOptions.baseUri;
+*/
+chai.should();
+chai.use(sinonChai);
+const sandbox = sinon.sandbox.create();
+
+// Module to test
+const phoenix = require('../../lib/phoenix');
+
+test('phoenix should respond to fetchCampaign', () => {
+  phoenix.should.respondTo('fetchCampaign');
+});
+
+test('phoenix should respond to isClosedCampaign', () => {
+  phoenix.should.respondTo('isClosedCampaign');
+});
+
+test('phoenix should call handleSuccess on successful post', async () => {
+  // setup
+  sandbox.spy(phoenix, 'parsePhoenixCampaign');
+
+  // test
+  await phoenix.fetchCampaign(7);
+  phoenix.parsePhoenixCampaign.should.have.been.called;
+});
