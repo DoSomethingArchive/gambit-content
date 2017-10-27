dc1f654882ef058e6f8d78b6a1a329cb6287ccaa
Aaron Schachter <aaronschachter@gmail.com>
2017-10-27 13:23:54 -0700

    Move templates logic into render-templates

--- app/routes/campaigns-single.js
+++ app/routes/campaigns-single.js
@@ -5,14 +5,14 @@ const express = require('express');
 // Middleware;
 const getPhoenixCampaignMiddleware = require('../../lib/middleware/campaigns-single/phoenix-campaign');
 const getContentfulKeywordsMiddleware = require('../../lib/middleware/campaigns-single/contentful-keywords');
-const getContentfulTemplatesMiddleware = require('../../lib/middleware/campaigns-single/contentful-campaigns');
+const getContentfulCampaignsMiddleware = require('../../lib/middleware/campaigns-single/contentful-campaigns');
 const renderTemplatesMiddleware = require('../../lib/middleware/campaigns-single/render-templates');
 
 const router = express.Router({ mergeParams: true });
 
 router.use(getPhoenixCampaignMiddleware());
 router.use(getContentfulKeywordsMiddleware());
-router.use(getContentfulTemplatesMiddleware());
+router.use(getContentfulCampaignsMiddleware());
 router.use(renderTemplatesMiddleware());
 
 module.exports = router;
