e13db481ab4bee193257591a203a847a0180869a
Aaron Schachter <aaronschachter@gmail.com>
2017-10-26 23:59:02 -0700

    Starts on render templates middleware

--- app/routes/campaigns-single.js
+++ app/routes/campaigns-single.js
@@ -6,11 +6,13 @@ const express = require('express');
 const getPhoenixCampaignMiddleware = require('../../lib/middleware/campaigns-single/phoenix-campaign');
 const getContentfulKeywordsMiddleware = require('../../lib/middleware/campaigns-single/contentful-keywords');
 const getContentfulTemplatesMiddleware = require('../../lib/middleware/campaigns-single/contentful-campaigns');
+const renderTemplatesMiddleware = require('../../lib/middleware/campaigns-single/render-templates');
 
 const router = express.Router({ mergeParams: true });
 
 router.use(getPhoenixCampaignMiddleware());
 router.use(getContentfulKeywordsMiddleware());
 router.use(getContentfulTemplatesMiddleware());
+router.use(renderTemplatesMiddleware());
 
 module.exports = router;
