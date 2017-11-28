9233d078367e3de5ae8297a0474927eaf258099f
Aaron Schachter <aaronschachter@gmail.com>
2017-11-28 09:59:30 -0800

    Removes log collection

--- config/logger.js
+++ config/logger.js
@@ -1,7 +1,6 @@
 'use strict';
 
 const logger = require('winston');
-require('winston-mongodb').MongoDB; // eslint-disable-line no-unused-expressions
 
 const WINSTON_LEVEL = process.env.LOGGING_LEVEL || 'info';
 
@@ -11,10 +10,6 @@ module.exports = function init(options) {
       new logger.transports.Console({
         prettyPrint: true,
         colorize: true,
-        level: WINSTON_LEVEL,
-      }),
-      new logger.transports.MongoDB({
-        db: options.dbUri,
         level: WINSTON_LEVEL,
       }),
     ],
