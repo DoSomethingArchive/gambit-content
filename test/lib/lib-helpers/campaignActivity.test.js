a470181a641cf0359ce58d8d226ba510025647d2
Aaron Schachter <aaronschachter@gmail.com>
2018-04-05 11:47:49 -0700

    Adds test for createTextPostFromReq

--- test/lib/lib-helpers/campaignActivity.test.js
+++ test/lib/lib-helpers/campaignActivity.test.js
@@ -9,9 +9,12 @@ const sinonChai = require('sinon-chai');
 const sinon = require('sinon');
 const httpMocks = require('node-mocks-http');
 
+const rogue = require('../../../lib/rogue');
 const stubs = require('../../utils/stubs');
 
 const mockDraft = stubs.getDraft();
+const mockDefaultCreatePayload = { id: stubs.getUserId() };
+const mockPost = stubs.getPost();
 
 // Module to test
 const activityHelper = require('../../../lib/helpers/campaignActivity');
@@ -23,6 +26,8 @@ const sandbox = sinon.sandbox.create();
 
 test.beforeEach((t) => {
   stubs.stubLogger(sandbox, logger);
+  sandbox.stub(rogue, 'createPost')
+    .returns(Promise.resolve(mockPost));
   t.context.req = httpMocks.createRequest();
   t.context.req.userId = stubs.getUserId();
   t.context.req.incoming_message = stubs.getRandomString();
@@ -39,6 +44,17 @@ test.afterEach((t) => {
   t.context = {};
 });
 
+// createPhotoPostFromReq
+test('createTextPostFromReq calls createPost with getCreateTextPostPayloadFromReq', async (t) => {
+  sandbox.stub(activityHelper, 'getDefaultCreatePayloadFromReq')
+    .returns(mockDefaultCreatePayload);
+
+  const result = await activityHelper.createTextPostFromReq(t.context.req);
+  activityHelper.getDefaultCreatePayloadFromReq.should.have.been.called;
+  rogue.createPost.should.have.been.calledWith(mockDefaultCreatePayload);
+  result.should.equal(mockPost);
+});
+
 // getDefaultCreatePayloadFromReq
 test('getDefaultCreatePayloadFromReq returns an object', (t) => {
   const result = activityHelper.getDefaultCreatePayloadFromReq(t.context.req);
@@ -51,7 +67,7 @@ test('getDefaultCreatePayloadFromReq returns an object', (t) => {
 // getCreatePhotoPostPayloadFromReq
 test('getCreatePhotoPostPayloadFromReq returns an object', (t) => {
   sandbox.stub(activityHelper, 'getDefaultCreatePayloadFromReq')
-    .returns({ userId: stubs.getUserId() });
+    .returns(mockDefaultCreatePayload);
   const result = activityHelper.getCreatePhotoPostPayloadFromReq(t.context.req);
   result.type.should.equal('photo');
   activityHelper.getDefaultCreatePayloadFromReq.should.have.been.called;
@@ -64,7 +80,7 @@ test('getCreatePhotoPostPayloadFromReq returns an object', (t) => {
 // getCreateTextPostPayloadFromReq
 test('getCreateTextPostPayloadFromReq returns an object', (t) => {
   sandbox.stub(activityHelper, 'getDefaultCreatePayloadFromReq')
-    .returns({ userId: stubs.getUserId() });
+    .returns(mockDefaultCreatePayload);
   const result = activityHelper.getCreateTextPostPayloadFromReq(t.context.req);
   result.type.should.equal('text');
   result.text.should.equal(t.context.req.incoming_message);
