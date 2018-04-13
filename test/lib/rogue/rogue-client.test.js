0839737b5540fd67d7c9c2d2eec13674574c7b89
Rafa <rapala61@gmail.com>
2018-04-13 14:25:51 -0500

    Removes code that attempted to create a singleton for the Rogue client and strategies. Turns out I would have to rely on exporting an instance and I particularly dont like that. No singleton for me.

--- test/lib/rogue/rogue-client.test.js
+++ test/lib/rogue/rogue-client.test.js
@@ -6,12 +6,12 @@ require('dotenv').config();
 const test = require('ava');
 const chai = require('chai');
 
-chai.should();
-const expect = chai.expect;
-
 // Module to test
 const RogueClient = require('../../../lib/rogue/rogue-client');
 const ClientCredentialsStrategy = require('../../../lib/rogue/strategies/client-credentials');
+
+chai.should();
+const expect = chai.expect;
 
 test('RogueService should throw if no strategy is passed', () => {
   expect(() => RogueClient.getInstance()).to.throw();
@@ -19,5 +19,5 @@ test('RogueService should throw if no strategy is passed', () => {
 
 test('RogueService should not throw if strategy is passed', () => {
   const oauthStrategy = ClientCredentialsStrategy.getInstance();
-  expect(() => RogueClient.getInstance({}, oauthStrategy)).not.to.throw();
+  expect(() => RogueClient.getInstance({}, [oauthStrategy])).not.to.throw();
 });
