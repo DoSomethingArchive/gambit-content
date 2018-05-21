9b55f5751cfd01035201125e830d75328db70bdb
Aaron Schachter <aaronschachter@gmail.com>
2018-05-21 16:47:17 -0700

    Render templates

--- app/routes/topics/single.js
+++ app/routes/topics/single.js
@@ -4,9 +4,13 @@ const express = require('express');
 
 // Middleware;
 const getTopicMiddleware = require('../../../lib/middleware/topics/single/topic-get');
+const getCampaignMiddleware = require('../../../lib/middleware/topics/single/campaign-get');
+const renderTemplatesMiddleware = require('../../../lib/middleware/topics/single/templates-render');
 
 const router = express.Router({ mergeParams: true });
 
 router.use(getTopicMiddleware());
+router.use(getCampaignMiddleware());
+router.use(renderTemplatesMiddleware());
 
 module.exports = router;
