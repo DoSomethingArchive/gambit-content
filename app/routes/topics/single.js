b69aa9a09efaeb83580fc968e627034d3fa69c78
Aaron Schachter <aaronschachter@gmail.com>
2018-05-22 12:12:26 -0700

    Fetches defaultTopicTriggers for a topicId

--- app/routes/topics/single.js
+++ app/routes/topics/single.js
@@ -4,12 +4,12 @@ const express = require('express');
 
 const getTopicMiddleware = require('../../../lib/middleware/topics/single/topic-get');
 const getCampaignMiddleware = require('../../../lib/middleware/topics/single/campaign-get');
-const renderTemplatesMiddleware = require('../../../lib/middleware/topics/single/templates-render');
+const formatResponseMiddleware = require('../../../lib/middleware/topics/single/templates-render');
 
 const router = express.Router({ mergeParams: true });
 
 router.use(getTopicMiddleware());
 router.use(getCampaignMiddleware());
-router.use(renderTemplatesMiddleware());
+router.use(formatResponseMiddleware());
 
 module.exports = router;
