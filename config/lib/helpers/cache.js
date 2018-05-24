4cbd5bc96e29f61afabd4ba053e1af500bf84c18
Aaron Schachter <aaronschachter@gmail.com>
2018-05-23 22:13:21 -0700

    Adds topics cache config

--- config/lib/helpers/cache.js
+++ config/lib/helpers/cache.js
@@ -5,4 +5,8 @@ module.exports = {
     name: 'campaigns',
     ttl: parseInt(process.env.GAMBIT_CAMPAIGNS_CACHE_TTL, 10) || 3600,
   },
+  topics: {
+    name: 'topics',
+    ttl: parseInt(process.env.GAMBIT_TOPICS_CACHE_TTL, 10) || 3600,
+  },
 };
