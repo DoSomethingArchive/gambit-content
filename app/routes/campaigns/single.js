02e6ef70d870a6358a1dad8c4c11d1ba8e0bcef5
Aaron Schachter <aaronschachter@gmail.com>
2018-05-30 13:50:49 -0700

    Fetches keywords from campaign helper fetchById

--- app/routes/campaigns/single.js
+++ app/routes/campaigns/single.js
@@ -4,14 +4,12 @@ const express = require('express');
 
 // Middleware;
 const getCampaignMiddleware = require('../../../lib/middleware/campaigns/single/campaign-get');
-const getKeywordsMiddleware = require('../../../lib/middleware/campaigns/single/keywords-get');
 const getTopicsMiddleware = require('../../../lib/middleware/campaigns/single/topics-get');
 const sendResponseMiddleware = require('../../../lib/middleware/campaigns/single/response');
 
 const router = express.Router({ mergeParams: true });
 
 router.use(getCampaignMiddleware());
-router.use(getKeywordsMiddleware());
 router.use(getTopicsMiddleware());
 router.use(sendResponseMiddleware());
 
