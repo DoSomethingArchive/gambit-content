85acb52b67e54b626d1888e616a2a6b710fbb5e8
Aaron Schachter <aaronschachter@gmail.com>
2018-07-19 22:51:27 -0700

    Rename reportbackHelper as utilHelper

--- test/lib/lib-helpers/util.test.js
+++ test/lib/lib-helpers/util.test.js
@@ -17,7 +17,7 @@ const stubs = require('../../utils/stubs');
 const config = require('../../../config/lib/helpers/util');
 
 // Module to test
-const reportbackHelper = require('../../../lib/helpers/util');
+const utilHelper = require('../../../lib/helpers/util');
 
 // setup "x.should.y" assertion style
 chai.should();
@@ -43,42 +43,42 @@ test.afterEach(() => {
 // isValidQuantity
 test('isValidQuantity() validations', () => {
   // non decimal number should pass
-  reportbackHelper.isValidQuantity('1').should.be.true;
-  reportbackHelper.isValidQuantity(' 1').should.be.true;
-  reportbackHelper.isValidQuantity(' 1 ').should.be.true;
+  utilHelper.isValidQuantity('1').should.be.true;
+  utilHelper.isValidQuantity(' 1').should.be.true;
+  utilHelper.isValidQuantity(' 1 ').should.be.true;
   // decimal number should not pass
-  reportbackHelper.isValidQuantity('1.1').should.be.false;
-  reportbackHelper.isValidQuantity(' 1.1').should.be.false;
-  reportbackHelper.isValidQuantity(' 1.1 ').should.be.false;
-  reportbackHelper.isValidQuantity(' 0.9 ').should.be.false;
+  utilHelper.isValidQuantity('1.1').should.be.false;
+  utilHelper.isValidQuantity(' 1.1').should.be.false;
+  utilHelper.isValidQuantity(' 1.1 ').should.be.false;
+  utilHelper.isValidQuantity(' 0.9 ').should.be.false;
   // any text should not pass
-  reportbackHelper.isValidQuantity('a').should.be.false;
-  reportbackHelper.isValidQuantity(' 1a').should.be.false;
-  reportbackHelper.isValidQuantity('1,').should.be.false;
-  reportbackHelper.isValidQuantity('1 hundred').should.be.false;
+  utilHelper.isValidQuantity('a').should.be.false;
+  utilHelper.isValidQuantity(' 1a').should.be.false;
+  utilHelper.isValidQuantity('1,').should.be.false;
+  utilHelper.isValidQuantity('1 hundred').should.be.false;
 });
 
 // isValidText
 test('isValidText() validations', () => {
   // Text can't be empty
-  reportbackHelper.isValidText('').should.be.false;
-  reportbackHelper.isValidText('       ').should.be.false;
+  utilHelper.isValidText('').should.be.false;
+  utilHelper.isValidText('       ').should.be.false;
   // Text must meet length requirements
-  reportbackHelper.isValidText(stubs.getRandomString(config.minTextLength + 1)).should.be.true;
-  reportbackHelper.isValidText(stubs.getRandomString(config.minTextLength)).should.be.false;
+  utilHelper.isValidText(stubs.getRandomString(config.minTextLength + 1)).should.be.true;
+  utilHelper.isValidText(stubs.getRandomString(config.minTextLength)).should.be.false;
 });
 
 // trimText
 test('trimText() validations', () => {
   // It should trim spaces out of text
   const text1 = stubs.getRandomString();
-  reportbackHelper.trimText(`   ${text1}   `).should.be.equal(text1);
+  utilHelper.trimText(`   ${text1}   `).should.be.equal(text1);
   // If longer that maxTextLength it should ellipsify
   const text2 = stubs.getRandomString(config.maxTextLength + 10);
-  const result2 = reportbackHelper.trimText(text2);
+  const result2 = utilHelper.trimText(text2);
   result2.indexOf(config.ellipsis).should.be.equal(result2.length - config.ellipsis.length);
   // Text shouldn't be longer than 255|maxTextLength
   const text3 = stubs.getRandomString(config.maxTextLength + 100);
-  const result3 = reportbackHelper.trimText(text3);
+  const result3 = utilHelper.trimText(text3);
   result3.length.should.be.equal(config.maxTextLength);
 });
