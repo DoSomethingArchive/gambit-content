f30b217c59ef28d1a103824826b4761887de473b
Aaron Schachter <aaronschachter@gmail.com>
2018-09-24 14:51:08 -0700

    Refactors with parsedContentfulEntries cache
    Adds tests

--- config/lib/helpers/cache.js
+++ config/lib/helpers/cache.js
@@ -1,24 +1,12 @@
 'use strict';
 
 module.exports = {
-  broadcasts: {
-    name: 'broadcasts',
-    ttl: parseInt(process.env.GAMBIT_BROADCASTS_CACHE_TTL, 10) || 3600,
-  },
-  campaignConfigs: {
-    name: 'campaignConfigs',
-    ttl: parseInt(process.env.GAMBIT_CAMPAIGN_CONFIGS_CACHE_TTL, 10) || 3600,
-  },
   campaigns: {
     name: 'campaigns',
     ttl: parseInt(process.env.GAMBIT_CAMPAIGNS_CACHE_TTL, 10) || 3600,
   },
-  defaultTopicTriggers: {
-    name: 'defaultTopicTriggers',
-    ttl: parseInt(process.env.GAMBIT_DEFAULT_TOPIC_TRIGGERS_CACHE_TTL, 10) || 3600,
-  },
-  topics: {
-    name: 'topics',
-    ttl: parseInt(process.env.GAMBIT_TOPICS_CACHE_TTL, 10) || 3600,
+  contentfulEntries: {
+    name: 'contentfulEntries',
+    ttl: parseInt(process.env.GAMBIT_CONTENTFUL_ENTRIES_CACHE_TTL, 10) || 3600,
   },
 };
