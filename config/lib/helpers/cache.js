e47481e0bdc4fcb2441a964075ab7c0582ad8d00
Aaron Schachter <aaronschachter@gmail.com>
2018-09-24 15:52:54 -0700

    Rename for consistency

--- config/lib/helpers/cache.js
+++ config/lib/helpers/cache.js
@@ -5,8 +5,8 @@ module.exports = {
     name: 'campaigns',
     ttl: parseInt(process.env.GAMBIT_CAMPAIGNS_CACHE_TTL, 10) || 3600,
   },
-  contentfulEntries: {
-    name: 'contentfulEntries',
-    ttl: parseInt(process.env.GAMBIT_CONTENTFUL_ENTRIES_CACHE_TTL, 10) || 3600,
+  parsedContentfulEntries: {
+    name: 'parsedContentfulEntries',
+    ttl: parseInt(process.env.GAMBIT_PARSED_CONTENTFUL_ENTRIES_CACHE_TTL, 10) || 3600,
   },
 };
