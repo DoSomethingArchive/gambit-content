41a9d4a46a7a0fb587b78eba23144a991fc141f6
Aaron Schachter <aaronschachter@gmail.com>
2018-11-15 20:25:25 -0800

    Removes fetching topics for single campaign request

--- app/routes/campaigns/single.js
+++ app/routes/campaigns/single.js
@@ -2,15 +2,11 @@
 
 const express = require('express');
 
-// Middleware;
+
 const getCampaignMiddleware = require('../../../lib/middleware/campaigns/single/campaign-get');
-const getTopicsMiddleware = require('../../../lib/middleware/campaigns/single/topics-get');
-const sendResponseMiddleware = require('../../../lib/middleware/campaigns/single/response');
 
 const router = express.Router({ mergeParams: true });
 
 router.use(getCampaignMiddleware());
-router.use(getTopicsMiddleware());
-router.use(sendResponseMiddleware());
 
 module.exports = router;
